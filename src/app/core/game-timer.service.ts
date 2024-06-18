eb7f45b2e00ebf3a7100bea294d4101dbef1bb75
Sean Gravener <sean@gravener.net>
2024-06-17 21:43:32 -0400

    Refactor

--- src/app/core/game-timer.service.ts
+++ src/app/core/game-timer.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, timer } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription, timer } from 'rxjs';
 import { map, takeWhile, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameTimerService {
-  private timer$: Observable<number> | undefined;
   private isRunning: boolean = false;
   private startTime: number = 0;
+  private subs = new Subscription();
 
   private currentTimeSubject = new BehaviorSubject<number>(0);
   public currentTime$ = this.currentTimeSubject.asObservable();
+
+  timer$ = timer(0, 1000).pipe(
+    takeWhile(() => this.isRunning),
+    map(() => Math.floor((Date.now() - this.startTime) / 1000)),
+    map((time) => time.toString()),
+    tap((time) => this.currentTimeSubject.next(parseInt(time, 10))),
+  );
+
+  get currentTime() {
+    return this.currentTimeSubject.getValue();
+  }
+
+  get isTimerRunning() {
+    return this.isRunning;
+  }
 
   constructor() {}
 
@@ -22,22 +37,21 @@ export class GameTimerService {
 
     this.isRunning = true;
     this.startTime = Date.now();
-    this.timer$ = timer(0, 1000).pipe(
-      map(() => Math.floor((Date.now() - this.startTime) / 1000)),
-      tap((time) => this.currentTimeSubject.next(time)),
-      takeWhile(() => this.isRunning)
-    );
-
-    this.timer$.subscribe();
+    this.subs.add(this.timer$.subscribe());
   }
 
   stopTimer(): void {
     this.isRunning = false;
-    this.currentTimeSubject.next(0);
+    this.currentTimeSubject.next(this.currentTime);
   }
 
   resetTimer(): void {
     this.stopTimer();
-    this.startTimer();
+    this.currentTimeSubject.next(0);
+  }
+
+  destroyTimer(): void {
+    this.stopTimer();
+    this.subs.unsubscribe();
   }
 }
